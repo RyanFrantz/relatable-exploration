39ee472415937bc5f86a4770a0a6066eb5fec836
Ryan Frantz <ryanleefrantz@gmail.com>
2022-12-31 16:38:31 -0500

    Cleans up my toy script.

--- src/dyntest.js
+++ src/dyntest.js
@@ -6,56 +6,72 @@ import AWS from 'aws-sdk';
 
 const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
+const handles = [
+ {
+   'pk': 'user|Ryan Frantz',
+   'sk': 'canonical|Ryan Frantz',
+   'createdAt': '20221231'
+ },
+ {
+   'pk': 'user|Ryan Frantz',
+   'sk': 'github|RyanFrantz',
+   'createdAt': '20221231'
+ },
+ {
+   'pk': 'user|Ryan Frantz',
+   'sk': 'twitter|Ryan_Frantz',
+   'createdAt': '20221231'
+ },
+ {
+   'pk': 'user|Ryan Frantz',
+   'sk': 'mastodon|ryan@very.social',
+   'createdAt': '20221231'
+ },
+];
 
-const params = {
+let params = {
   TableName: 'sandbox-20221229',
-  Item: {
-    'pk': 'user#Ryan Frantz',
-    //'sk': 'profile#Ryan Frantz'
-    //'sk': 'github#RyanFrantz'
-    //'sk': 'twitter#Ryan_Frantz'
-    'sk': 'mastodon#ryan@very.social',
-    'createdAt': '20221229'
-  }
 };
 
 try {
-  await docClient.put(params).promise();
-  console.log('Successfully added item!');
+  for (const h of handles) {
+    params.Item = h;
+    await docClient.put(params).promise();
+    console.log('Successfully added item!');
+  };
 } catch (err) {
   console.log('Error ', err);
 }
-/*
-*/
 
 /* If an attribute name matches a reserved word (like 'user') or includes
  * a special character like '.' or '-', we have to use expression attribute
  * names and values to create our query!
  */
-const queryParams = {
-  TableName: 'sandbox-20221229',
-  //KeyConditionExpression: 'pk = :user',
-  KeyConditionExpression: 'pk = :user and begins_with (sk, :handleType)',
-  ExpressionAttributeValues: {
-    ':user': 'user#Ryan Frantz',
-    ':handleType': 'github'
-  }
+const queryParams = [
+  {
+    TableName: 'sandbox-20221229',
+    KeyConditionExpression: 'pk = :user and begins_with (sk, :handleType)',
+    ExpressionAttributeValues: {
+      ':user': 'user|Ryan Frantz',
+      ':handleType': 'github'
+    }
+  },
+  {
+    TableName: 'sandbox-20221229',
+    IndexName: 'createdAt-sk-index',
+    KeyConditionExpression: 'createdAt = :date',
+    ExpressionAttributeValues: {
+      ':date': '20221231'
+    }
 }
-
-/*
-const queryParams = {
-  TableName: 'sandbox-20221229',
-  IndexName: 'createdAt-sk-index',
-  KeyConditionExpression: 'createdAt = :date',
-  ExpressionAttributeValues: {
-    ':date': '20221229'
-  }
-}
-*/
+]
 
 try {
-  const items = await docClient.query(queryParams).promise();
-  console.log(items);
+  for (const params of queryParams) {
+    console.log('Query Params:', params)
+    const items = await docClient.query(params).promise();
+    console.log(items);
+  }
 } catch (err) {
   console.log("Error", err);
 }
