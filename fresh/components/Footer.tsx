902e641dbf1f7a2fb61a00f46249ad7fc5ee7de2
Ryan Frantz <ryanleefrantz@gmail.com>
2023-02-11 13:38:25 -0500

    Made with Fresh

--- fresh/components/Footer.tsx
+++ fresh/components/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <footer>
-      Copyright 2023 Ryan Frantz
+      <h6>Copyright Ryan Frantz</h6>
+      <a href="https://fresh.deno.dev">
+         <img width="197" height="37" src="https://fresh.deno.dev/fresh-badge.svg" alt="Made with Fresh" />
+      </a>
     </footer>
   );
 }
