781010a79e5fb61208015596839411ae0148c914
Ryan Frantz <ryanleefrantz@gmail.com>
2023-02-11 09:31:57 -0500

    Leaves deploy breadcrumb.

--- fresh/main.ts
+++ fresh/main.ts
@@ -4,6 +4,10 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
+/*
+ * Deployed via
+ * DENO_DEPLOY_TOKEN=... deployctl deploy --project=relatable --prod main.ts
+ */
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
