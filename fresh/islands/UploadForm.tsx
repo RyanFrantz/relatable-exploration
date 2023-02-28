850e5cf47e457b71455b8be74696acbc68eeaa23
Ryan Frantz <ryanleefrantz@gmail.com>
2023-02-27 19:22:51 -0500

    Parse CSV and show simple preview.

--- fresh/islands/UploadForm.tsx
+++ fresh/islands/UploadForm.tsx
@@ -1,6 +1,16 @@
+import { useState } from "preact/hooks";
 import Papa from 'https://esm.sh/papaparse@5.3.2';
 
 export default function UploadForm() {
+  const [uploadedData, setUploadedData] = useState();
+
+  // https://stackoverflow.com/questions/2970525/converting-any-string-into-camel-case
+  function camelize(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+  }
+
   // With a non-submit button, I expect we won't see this get called.
   const handleSubmit = (e) => {
     // Prevent form submission. Useful to do things on the same page.
@@ -12,23 +22,47 @@ export default function UploadForm() {
     const file = event.target.files[0];
     console.log('File to upload: ', file);
     Papa.parse(file, {
-      header: true,
+      header: true, // Organize by header.
       skipEmptyLines: true,
+      transformHeader: function(header) {
+        return camelize(header); // "Employment Status" -> "employmentStatus"
+      },
       complete: function(results) {
         // TODO: Set state here and use it when the upload button is clicked.
+        //console.log(results.data);
+        // results.meta.fields contains the header names.
         console.log(results.data);
+        setUploadedData(results.data);
       }
     });
   };
 
   const handleUpload = () => {
     console.log('Upload starting...');
+    // We'll call our API from here.
   };
 
   return (
+    <>
     <form onSubmit={handleSubmit}>
+      <p>
+        Your CSV should contain two headers: "Name" and "Employment Status".
+      </p>
       <input id="csv-input-file" type="file" onChange={handleChange}/>
       <button type="button" onClick={handleUpload}>Upload CSV</button>
     </form>
+    {uploadedData ? (
+    <div>
+      {/* Replace this with a component. */}
+      {
+        uploadedData.map(
+          ({name, employmentStatus}) => <p>{`${name} ${employmentStatus}`}</p>)
+      }
+    </div>
+    ) : (
+      <div></div>
+    )
+    }
+    </>
   );
 }
