d46ef5ee92eda651246adc6e1b5e7718ee6d5bb6
Ryan Frantz <ryanleefrantz@gmail.com>
2023-03-21 07:57:21 -0400

    Shift POST support for handles.

--- fresh/routes/api/handle/[handle].ts
+++ fresh/routes/api/handle/[handle].ts
@@ -1,10 +1,6 @@
 import { Handlers } from "$fresh/server.ts";
 import { connect } from 'https://esm.sh/*@planetscale/database@1.4.0';
 
-// Insert a single user handle.
-// curl -v -X POST localhost:5000/api/handle \
-//   -d '{}'
-
 // Planetscale config.
 // Env vars are expected to be defined in the Deno Deploy project.
 const pConfig = {
@@ -14,13 +10,6 @@ const pConfig = {
 };
 
 const conn = connect(pConfig);
-
-interface Handle {
-  username: string;
-  userId: number;
-  handle: string;
-  handleType: string;
-}
 
 // Given a handle, return the related user.
 const getUserByHandle = async (handle: string) => {
@@ -32,46 +21,8 @@ const getUserByHandle = async (handle: string) => {
   return user;
 };
 
-// Test that a user exists.
-// Planetscale/Vitess don't support foreign key constraints so this check must
-// live in the app.
-const getUserId = async (name: string) => {
-  const stmt = `SELECT id FROM user WHERE name = :name LIMIT 1`;
-  const result = await conn.execute(stmt, {name: name});
-  if (result.rows?.length > 0) {
-    console.log(result);
-    return result.rows[0].id;
-  }
-  return false; // Sane default.
-};
-
-// Inserts a user handle into the database.
-// Returns the HTTP response and a helpful string message.
-const insertHandle = async (handle: Handle): [number, string] => {
-  const userId = await getUserId(handle.username);
-  if (!userId) {
-    return [404, `User ${handle.username} not found.`];
-  }
-  handle.userId = userId;
-  // The combination of user_id, handle, and handleType is unique.
-  // Do nothing when a handle already exists.
-  const stmt = `
-    INSERT INTO handles (user_id, handle, handleType)
-    VALUES(:userId, :handle, :handleType)
-    ON DUPLICATE KEY UPDATE id=id
-  `;
-
-  const results = await conn.execute(stmt, handle);
-  if (results.insertId) {
-    // Created
-    return [201, `Created handle ${handle.handle} of type ${handle.handleType} for user ${handle.username}!`];
-  } else {
-    // Conflict
-    return [409, `Handle ${handle.handle} of type ${handle.handleType} already exists for user ${handle.username}.`];
-  }
-};
-
 export const handler: Handlers = {
+  // Given a handle, look up the related user.
   // /api/handle/foo, /api/handle/fancy%pants
   async GET(req) {
     const url = new URL(req.url);
@@ -106,23 +57,5 @@ export const handler: Handlers = {
       }
     );
   },
-  // TODO: Enforce the expected shape of the body in this request.
-  async POST(req) {
-    let body;
-    try {
-      // TODO: Validate the body structure meets our expectation.
-      body = await req.json();
-    } catch (err) {
-      console.log('Error: ', err.message);
-      return new Response(JSON.stringify({message: 'Invalid input!'}), { status: 400 });
-    }
-    // TODO: De-taint input.
-    const [responseCode, msg] = await insertHandle(body);
-    return new Response(
-      JSON.stringify({message: msg}), {
-        status: responseCode
-      }
-    );
-  }
 };
 
--- fresh/routes/api/user/[id]/handles.ts
+++ fresh/routes/api/user/[id]/handles.ts
@@ -19,6 +19,48 @@ const getHandles = async (userId: number) => {
   return result.rows;
 }
 
+interface Handle {
+  handle: string;
+  handleType: string;
+}
+
+// Test that a user exists.
+// Planetscale/Vitess don't support foreign key constraints so this check must
+// live in the app.
+const userExists = async (id: number) => {
+  const stmt = `SELECT id FROM user WHERE id = :id`;
+  const result = await conn.execute(stmt, {id: id});
+  if (result.rows?.length > 0) {
+    return true;
+  }
+  return false; // Sane default.
+};
+
+// Inserts a user handle into the database.
+// Returns the HTTP response and a helpful string message.
+const insertHandle = async (userId: number, handle: Handle): [number, string] => {
+  if (!userExists(userId)) {
+    return [404, `User ID ${userId} not found.`];
+  }
+  handle.userId = userId;
+  // The combination of user_id, handle, and handleType is unique.
+  // Do nothing when a handle already exists.
+  const stmt = `
+    INSERT INTO handles (user_id, handle, handleType)
+    VALUES(:userId, :handle, :handleType)
+    ON DUPLICATE KEY UPDATE id=id
+  `;
+
+  const results = await conn.execute(stmt, handle);
+  if (results.insertId) {
+    // Created
+    return [201, `Created handle ${handle.handle} of type ${handle.handleType} for user ID ${handle.userId}!`];
+  } else {
+    // Conflict
+    return [409, `Handle ${handle.handle} of type ${handle.handleType} already exists for user ID ${handle.userId}.`];
+  }
+};
+
 export const handler: Handlers = {
   // curl -sv http://localhost:5000/api/user/7/handles
   async GET(req) {
@@ -43,4 +85,38 @@ export const handler: Handlers = {
       }
     );
   },
+  // Given the following request body, create a handle record for a user:
+  // { "handle": "FairyGodmother", handleType: "twitter" }
+  async POST(req) {
+    const url = new URL(req.url);
+    // Helps ensure we only match numeric IDs, not all strings.
+    const re = /\/api\/user\/(?<userId>\d+)\/handles/;
+    const match = url.pathname.match(re);
+    if (!match) {
+      return new Response(
+        JSON.stringify({message: 'Invalid user ID'}),
+        {
+          status: 400
+        }
+      );
+    }
+    const userId = match.groups.userId;
+
+    let body: Handle;
+    try {
+      // TODO: Validate the body structure meets our expectation.
+      body = await req.json();
+    } catch (err) {
+      console.log('Error: ', err.message);
+      return new Response(JSON.stringify({message: 'Invalid input!'}), { status: 400 });
+    }
+    // TODO: De-taint input.
+    const [responseCode, msg] = await insertHandle(userId, body);
+    return new Response(
+      JSON.stringify({message: msg}), {
+        status: responseCode
+      }
+    );
+  }
 };
+
